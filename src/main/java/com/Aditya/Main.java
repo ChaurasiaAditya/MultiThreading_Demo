f09a011a08e8cdab33d6729675757f313239d0ca
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2023-03-29 15:34:58 +0530

    demo with 2 Threads

--- src/main/java/com/Aditya/Main.java
+++ src/main/java/com/Aditya/Main.java
@@ -1,7 +1,21 @@
 package com.Aditya;
 
+import com.Aditya.ByInheritence.MultiThreadOne;
+
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello world!");
+
+		//		With Two Threads
+		/*
+		MultiThreadOne multiThreadOne = new MultiThreadOne();
+		MultiThreadOne multiThreadOne1 = new MultiThreadOne();
+		multiThreadOne.start();
+		multiThreadOne1.start();
+		*/
+
+
+		// With
+
+
 	}
 }
